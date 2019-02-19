578a0e8c5c4cb6ab1e57b852a5f21ddb4448f051
Alex <rainsiphoner@gmail.com>
2019-02-18 19:33:39 -0500

    basic GUI is almost done requires functionality assistance

--- src/main/java/frc/CustomDashboard.java
+++ src/main/java/frc/CustomDashboard.java
@@ -9,6 +9,7 @@ package frc;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -38,6 +39,15 @@ public class CustomDashboard {
     private JMenuBar mb;
     private JTextArea ta;
     private JLabel[] armPositions;
+    private JLabel[] diagnostics;
+    private JLabel[] sensorOutputs;
+    private int wristamperage;
+    private int intakeamperage;
+   // private int armamperage;
+    private double pdpCurrent;
+    private int encoderValue;
+    private double ultrasonicValue;
+    
 
     public CustomDashboard(){
         //Creating the Frame
@@ -58,24 +68,43 @@ public class CustomDashboard {
         JPanel logPanel = new JPanel(); // thelogPanel is not visible in output
         JTextField tf = new JTextField(30);
        logPanel.add(tf);
+       
 
        // panels for Ultra Gooey mode
         JPanel armPanel = new JPanel(new GridLayout(9, 1));
         JPanel positionPanel = new JPanel();
         JPanel cameraPanel = new JPanel();
-        JPanel statusPanel = new JPanel();
+        JPanel statusPanel = new JPanel(new BorderLayout());
         JPanel pathPanel = new JPanel();
-
+        JPanel sensorPanel = new JPanel(new GridLayout(4, 1));
+        JPanel diagnosticPanel = new JPanel(new GridLayout(2, 3));
+        
         driveFrame.getContentPane().add(BorderLayout.CENTER, positionPanel);
         driveFrame.getContentPane().add(BorderLayout.EAST, armPanel);
         driveFrame.getContentPane().add(BorderLayout.SOUTH, statusPanel);
         positionPanel.add(pathPanel);
         positionPanel.add(cameraPanel);
+        statusPanel.add(BorderLayout.WEST, sensorPanel);
+        statusPanel.add(BorderLayout.CENTER, diagnosticPanel);
+        statusPanel.setBackground(Color.BLACK);
+        positionPanel.setBackground(Color.BLACK);
         
-        Border border = BorderFactory.createLineBorder(Color.green, 5);
-
-        
+        Border border = BorderFactory.createLineBorder(Color.GREEN, 5);
+        Border miniborder = BorderFactory.createLineBorder(Color.GREEN, 1);
+        Font font = new Font("Serif", Font.BOLD, 18);
+        tf.setFont(font);
+        tf.setForeground(Color.GREEN);
         // positions count down from top to bottom, like any standard rows
+
+        //order is super important here
+        
+        
+        
+        sensorOutputs = new JLabel[2];
+        sensorOutputs[0] = new JLabel("Encoder Value: " + encoderValue);
+        sensorOutputs[1] = new JLabel("Ultrasonic Value: " + ultrasonicValue);
+        
+
 
         armPositions = new JLabel[9];
         armPositions[0] = new JLabel("3 Ball Reversed");
@@ -88,15 +117,47 @@ public class CustomDashboard {
         armPositions[7] = new JLabel("1 Hatch Standard");
         armPositions[8] = new JLabel("0 (Intake Mode)");
 
+        diagnostics = new JLabel[6];
+        diagnostics[0] = new JLabel("Potentiometer");
+        diagnostics[1] = new JLabel("PDP");
+        diagnostics[2] = new JLabel("Wrist");
+        diagnostics[3] = new JLabel("Intake");
+        diagnostics[4] = new JLabel("Arm");
+        diagnostics[5] = new JLabel("Compressor");
+
+        for(int i = 0; i < diagnostics.length; i++){
+            diagnosticPanel.add(diagnostics[i]);
+            diagnosticPanel.setBorder(border);
+            diagnosticPanel.setOpaque(true);
+            diagnosticPanel.setBackground(Color.BLACK);
+            diagnostics[i].setOpaque(true);
+            diagnostics[i].setBackground(Color.BLACK);
+            diagnostics[i].setForeground(Color.GREEN);
+            diagnostics[i].setBorder(miniborder);
+            diagnostics[i].setFont(font);
+
+        }
+
         for(int i = 0; i < armPositions.length; i++){
             armPanel.add(armPositions[i]);
             armPanel.setBorder(border);
+            armPanel.setOpaque(true);
+            armPanel.setBackground(Color.BLACK);
             armPositions[i].setOpaque(true);
             armPositions[i].setBackground(Color.BLACK);
             armPositions[i].setForeground(Color.GREEN);
+            armPositions[i].setBorder(miniborder);
+            armPositions[i].setFont(font);
         }
         
-
+        for(int i = 0; i < sensorOutputs.length; i++){
+            sensorPanel.add(sensorOutputs[i]);
+            sensorPanel.setOpaque(true);
+            sensorPanel.setBackground(Color.BLACK);
+            sensorPanel.setBorder(border);
+            sensorOutputs[i].setFont(font);
+            sensorOutputs[i].setForeground(Color.GREEN);
+        }
 
 
         tf.addKeyListener(new KeyListener(){
@@ -149,6 +210,56 @@ public class CustomDashboard {
         
     }
 
+
+    public void potentiometerValue(boolean on){
+        if(on == true){
+            diagnostics[0].setBackground(Color.GREEN);
+            diagnostics[0].setForeground(Color.BLACK);
+        }else{
+            diagnostics[0].setBackground(Color.RED);
+            diagnostics[0].setForeground(Color.WHITE);
+        }
+    }
+
+    public void pdpValue(boolean toomuch){
+        if( 1 > pdpCurrent || pdpCurrent > 4){
+            diagnostics[1].setBackground(Color.RED);
+        }else{
+            diagnostics[1].setBackground(Color.GREEN);
+        }
+
+    }
+
+    public void wristValue(boolean toomuch){
+
+        if(wristamperage >= 2){
+            diagnostics[2].setBackground(Color.RED);
+        }else{
+            diagnostics[2].setBackground(Color.GREEN);
+        }
+
+    }
+
+    public void intakeValue(boolean toomuch){
+        if(intakeamperage >= 2){
+            diagnostics[3].setBackground(Color.RED);
+        }else{
+            diagnostics[3].setBackground(Color.GREEN);
+        }
+    }
+/*
+    public void armValue(boolean toomuch){
+        if(armamperage >= 2){
+            diagnostics[4].setBackground(Color.RED);
+        }else{
+            if(motor.get() = 0 && (potentiometer.get() > 0 || encoder.get() > 0)){
+                diagnostics[4].setBackground(Color.RED);
+            } else {
+                diagnostics[4].setBackground(Color.GREEN);
+            }
+        }
+    }
+*/
     // positions count down from top to bottom, like any standard rows
     public void changePosition(int pos){
         for(int i = 0; i < armPositions.length; i++){
@@ -157,9 +268,27 @@ public class CustomDashboard {
             armPositions[pos].setBackground(Color.GREEN);
             armPositions[pos].setForeground(Color.BLACK);
         }
-
-
     }
+
+    public void changePositionLow(int pos){
+        for(int i = 0; i < armPositions.length; i++){
+            armPositions[i].setBackground(Color.BLACK);
+            armPositions[i].setForeground(Color.GREEN);
+            armPositions[pos].setBackground(new Color(100, 0, 20));
+            armPositions[pos].setForeground(Color.BLACK);
+
+        }
+    }
+
+    public void changePositionHigh(int pos){
+        for(int i = 0; i < armPositions.length; i++){
+            armPositions[i].setBackground(Color.BLACK);
+            armPositions[i].setForeground(Color.GREEN);
+            armPositions[pos].setBackground(new Color(255, 0, 20));
+            armPositions[pos].setForeground(Color.BLACK);
+        }
+    }
+
 
 
     public void logMessage(String msg){
@@ -212,11 +341,13 @@ public class CustomDashboard {
 
 
     // positions count down from top to bottom, like any standard rows
+
     public static void main(String args[]) {
         CustomDashboard cd = new CustomDashboard();
         cd.logMessage("test1");
         cd.logMessage("test2");
         cd.changePosition(4);
+        cd.potentiometerValue(false);
     }
 
 }
